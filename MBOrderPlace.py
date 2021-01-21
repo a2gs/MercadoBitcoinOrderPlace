7472b969a22072caa6f8d5572dd494802d3da3d4
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2021-01-20 21:32:45 -0300

    Add files via upload

--- MBOrderPlace.py
+++ MBOrderPlace.py
@@ -56,37 +56,44 @@ def runOrder(conn, argOrderType : str, argCoin : str, argValue : str) -> bool:
 	}
 
 	try:
-		conn.request("POST", REQUEST_PATH, params, headersOrder)
+		conn.request("POST", REQUEST_PATH, param, headersOrder)
 
 		response = conn.getresponse()
 		response = response.read()
 
 		response_json = json.loads(response)
 		print("Ordem executada! Resultado:")
+		print(json.dumps(response_json, indent=4))
 
-		print(f"Order Id: [{response_json['response_data']['order']['order_id']}]")
-		print(f"Coin Pair: [{response_json['response_data']['order']['coin_pair']}]")
-		print(f"Order Type: [{response_json['response_data']['order']['order_type']}] (1 - buy; 2 - sell)")
-		print(f"Status: [{response_json['response_data']['order']['status']}] (2 - open; 3 - canceled; 4 - filled)")
-		print(f"Execucoes: [{response_json['response_data']['order']['has_fills']}] (false - Sem execucoes; true - Com uma ou mais execucoes)")
-		print(f"Quantidade: [{response_json['response_data']['order']['quantity']}]")
-		print(f"Preco Limite: [{response_json['response_data']['order']['limit_price']}]")
-		print(f"Qtd Executada: [{response_json['response_data']['order']['executed_quantity']}]")
-		print(f"Preco Medio Executada: [{response_json['response_data']['order']['executed_price_avg']}]")
-		print(f"Taxa: [{response_json['response_data']['order']['fee']}]")
-		print(f"Timestamp: [{response_json['response_data']['order']['created_timestamp']}]")
-		print(f"Updated Timestamp: [{response_json['response_data']['order']['updated_timestamp']}]")
-		print(f"Operacao:")
-		print(f"\tId: [{response_json['response_data']['order']['operations']['operation_id']}]")
-		print(f"\tQtd: [{response_json['response_data']['order']['operations']['quantity']}]")
-		print(f"\tPreco: [{response_json['response_data']['order']['operations']['price']}]")
-		print(f"\tFee: [{response_json['response_data']['order']['operations']['fee_rate']}]")
-		print(f"\tTimestamp: [{response_json['response_data']['order']['operations']['executed_timestamp']}]")
+		print(f"Status: [{response_json['status_code']}]", end = ' ')
 
-		print(f"Status: [{response_json['status_code']}]")
+		if response_json['status_code'] == 100:
+			print("Sucesso!")
+
+			print(f"Order Id: [{response_json['response_data']['order']['order_id']}]")
+			print(f"Coin Pair: [{response_json['response_data']['order']['coin_pair']}]")
+			print(f"Order Type: [{response_json['response_data']['order']['order_type']}] (1 - buy; 2 - sell)")
+			print(f"Status: [{response_json['response_data']['order']['status']}] (2 - open; 3 - canceled; 4 - filled)")
+			print(f"Execucoes: [{response_json['response_data']['order']['has_fills']}] (false - Sem execucoes; true - Com uma ou mais execucoes)")
+			print(f"Quantidade: [{response_json['response_data']['order']['quantity']}]")
+			print(f"Preco Limite: [{response_json['response_data']['order']['limit_price']}]")
+			print(f"Qtd Executada: [{response_json['response_data']['order']['executed_quantity']}]")
+			print(f"Preco Medio Executada: [{response_json['response_data']['order']['executed_price_avg']}]")
+			print(f"Taxa: [{response_json['response_data']['order']['fee']}]")
+			print(f"Timestamp: [{response_json['response_data']['order']['created_timestamp']}]")
+			print(f"Updated Timestamp: [{response_json['response_data']['order']['updated_timestamp']}]")
+			print(f"Operacao:")
+			print(f"\tId: [{response_json['response_data']['order']['operations']['operation_id']}]")
+			print(f"\tQtd: [{response_json['response_data']['order']['operations']['quantity']}]")
+			print(f"\tPreco: [{response_json['response_data']['order']['operations']['price']}]")
+			print(f"\tFee: [{response_json['response_data']['order']['operations']['fee_rate']}]")
+			print(f"\tTimestamp: [{response_json['response_data']['order']['operations']['executed_timestamp']}]")
+
+		else:
+			print("Erro!")
+			print(f"Mesage: [{response_json['error_message']}]")
+
 		print(f"Server Timestamp: [{response_json['server_unix_timestamp']}]")
-
-		#print(json.dumps(response_json, indent=4))
 
 		return True
 
@@ -112,9 +119,15 @@ def getPrice(conn, coin : str) -> [bool, float]:
 
 
 if __name__ == '__main__':
-	if len(argv) != 5:
+	if len(argv) != 6:
 		print(f"Uso:\n\t{argv[0]} [BUY / SELL] [MOEDA] [VALOR_REAIS / QTD_MOEDA] [MAIOR / MENOR / IGUAL] [VALOR_DE_REFERENCIA]")
 		exit(1)
+
+	argOrderType = argv[1].upper()
+	argCoin      = argv[2].upper()
+	argValue     = argv[3]
+	argCompare   = argv[4].upper()
+	argReference = float(argv[5])
 
 	if argCompare not in ['MAIOR', 'MENOR', 'IGUAL']:
 		print(f"Erro. Comparacoes permitidas: 'MAIOR', 'MENOR' ou 'IGUAL'. Comparacao '{argCompare}' nao reconhecida")
@@ -123,12 +136,6 @@ if __name__ == '__main__':
 	if argOrderType not in ['BUY', 'SELL']:
 		print(f"Erro. Comparacoes permitidas: 'BUY' ou 'SELL'. Comparacao '{argOrderType}' nao reconhecida")
 		exit(1)
-
-	argOrderType = argv[1].upper()
-	argCoin      = argv[2].upper()
-	argValue     = argv[3]
-	argCompare   = argv[4].upper()
-	argReference = argv[5]
 
 	try:
 		conn = client.HTTPSConnection('www.mercadobitcoin.net')
@@ -147,21 +154,22 @@ if __name__ == '__main__':
 			print("Erro no request do ticker.")
 			exit(1)
 
-		if argCompare == 'MAIOR' and price < argPrice:
+		if argCompare == 'MAIOR' and price < argReference:
 			ret = runOrder(conn, argOrderType, argCoin, argValue)
 			break
 
-		elif argCompare == 'MENOR' and price > argPrice:
+		elif argCompare == 'MENOR' and price > argReference:
 			ret = runOrder(conn, argOrderType, argCoin, argValue)
 			break
 
-		elif argCompare == 'IGUAL' and price == argPrice:
+		elif argCompare == 'IGUAL' and price == argReference:
 			ret = runOrder(conn, argOrderType, argCoin, argValue)
 			break
 
 		else:
 			pass
 
+		print(f"sleep... Last: {price} | Reference {argReference}")
 		time.sleep(2)
 
 	exit(0)
