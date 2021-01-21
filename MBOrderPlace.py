24f02788a14e42545a3e6495a6dc09633076d25a
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2021-01-21 02:35:31 -0300

    Update MBOrderPlace.py

--- MBOrderPlace.py
+++ MBOrderPlace.py
@@ -69,23 +69,23 @@ def runOrder(conn, argOrderType : str, argCoin : str, argValue : str) -> bool:
 		if response_json['status_code'] == 100:
 			print("Sucesso!")
 
-			print(f"Order Id: [{response_json['response_data']['order']['order_id']}]")
-			print(f"Coin Pair: [{response_json['response_data']['order']['coin_pair']}]")
-			print(f"Order Type: [{response_json['response_data']['order']['order_type']}] (1 - buy; 2 - sell)")
-			print(f"Status: [{response_json['response_data']['order']['status']}] (2 - open; 3 - canceled; 4 - filled)")
-			print(f"Execucoes: [{response_json['response_data']['order']['has_fills']}] (false - Sem execucoes; true - Com uma ou mais execucoes)")
-			print(f"Quantidade: [{response_json['response_data']['order']['quantity']}]")
-			print(f"Preco Limite: [{response_json['response_data']['order']['limit_price']}]")
-			print(f"Qtd Executada: [{response_json['response_data']['order']['executed_quantity']}]")
+			print(f"Order Id.............: [{response_json['response_data']['order']['order_id']}]")
+			print(f"Coin Pair............: [{response_json['response_data']['order']['coin_pair']}]")
+			print(f"Order Type...........: [{response_json['response_data']['order']['order_type']}] (1 - buy; 2 - sell)")
+			print(f"Status...............: [{response_json['response_data']['order']['status']}] (2 - open; 3 - canceled; 4 - filled)")
+			print(f"Execucoes............: [{response_json['response_data']['order']['has_fills']}] (false - Sem execucoes; true - Com uma ou mais execucoes)")
+			print(f"Quantidade...........: [{response_json['response_data']['order']['quantity']}]")
+			print(f"Preco Limite.........: [{response_json['response_data']['order']['limit_price']}]")
+			print(f"Qtd Executada........: [{response_json['response_data']['order']['executed_quantity']}]")
 			print(f"Preco Medio Executada: [{response_json['response_data']['order']['executed_price_avg']}]")
-			print(f"Taxa: [{response_json['response_data']['order']['fee']}]")
-			print(f"Timestamp: [{response_json['response_data']['order']['created_timestamp']}]")
-			print(f"Updated Timestamp: [{response_json['response_data']['order']['updated_timestamp']}]")
+			print(f"Taxa.................: [{response_json['response_data']['order']['fee']}]")
+			print(f"Timestamp............: [{response_json['response_data']['order']['created_timestamp']}]")
+			print(f"Updated Timestamp....: [{response_json['response_data']['order']['updated_timestamp']}]")
 			print(f"Operacao:")
-			print(f"\tId: [{response_json['response_data']['order']['operations']['operation_id']}]")
-			print(f"\tQtd: [{response_json['response_data']['order']['operations']['quantity']}]")
-			print(f"\tPreco: [{response_json['response_data']['order']['operations']['price']}]")
-			print(f"\tFee: [{response_json['response_data']['order']['operations']['fee_rate']}]")
+			print(f"\tId.......: [{response_json['response_data']['order']['operations']['operation_id']}]")
+			print(f"\tQtd......: [{response_json['response_data']['order']['operations']['quantity']}]")
+			print(f"\tPreco....: [{response_json['response_data']['order']['operations']['price']}]")
+			print(f"\tFee......: [{response_json['response_data']['order']['operations']['fee_rate']}]")
 			print(f"\tTimestamp: [{response_json['response_data']['order']['operations']['executed_timestamp']}]")
 
 		else:
@@ -97,7 +97,7 @@ def runOrder(conn, argOrderType : str, argCoin : str, argValue : str) -> bool:
 		return(True)
 
 	except Exception as e:
-		print(f"Erro colocando ordem {param}: {e}")
+		print(f"Erro colocando ordem [{param}]: [{e}]")
 		return(False)
 
 
